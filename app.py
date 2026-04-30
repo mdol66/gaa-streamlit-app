f703438f312c7a2af77838cf40444cb12d5493bd
mdol66 <mgd066@gmail.com>
2026-04-30 13:21:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -692,9 +692,9 @@ with tab1:
             hide_index=True,
             column_config={
                 "Outcome": st.column_config.TextColumn(width="small"),
-                "1(L)": st.column_config.NumberColumn(width="small"),
-                "2(M)": st.column_config.NumberColumn(width="small"),
-                "3(R)": st.column_config.NumberColumn(width="small"),
+                "1(L)": st.column_config.NumberColumn(width=50),
+                "2(M)": st.column_config.NumberColumn(width=50),
+                "3(R)": st.column_config.NumberColumn(width=50),
             }
         )
 
