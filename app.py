6a89d34fbcbb47810a63b7e477ff8ce88099e468
mdol66 <mgd066@gmail.com>
2026-04-30 13:14:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -689,6 +689,7 @@ with tab1:
         st.dataframe(
             channel_table,
             use_container_width=True,
+            hide_index=True,
             column_config={
                 "Outcome": st.column_config.TextColumn(width="small"),
                 "1(L)": st.column_config.NumberColumn(width="small"),
