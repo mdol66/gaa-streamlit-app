c9086f5c87f2ada047cd19d654eeab3054580f42
mdol66 <mgd066@gmail.com>
2026-05-15 13:28:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1014,7 +1014,11 @@ with left_col:
                     }
 
                 bt_metrics = calc_team_metrics(bt_df)
-                opp_metrics = calc_team_metrics(opp_df)
+                opp_metrics = {
+                    "to_won": bt_metrics["to_lost"],
+                    "to_lost": bt_metrics["to_won"],
+                    "net_to": bt_metrics["to_lost"] - bt_metrics["to_won"]
+                }
 
                 comparison_df = pd.DataFrame({
                     "Ballintubber": [
