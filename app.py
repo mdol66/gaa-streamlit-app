59c3106df031d2b13eb9a54b48140ad1b776f562
mdol66 <mgd066@gmail.com>
2026-05-15 13:15:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1090,13 +1090,13 @@ with mid_col:
                 ]
                 })
 
-                st.dataframe(
-                    comparison_df[
-                        ["Ballintubber", "Metric", opp_name if "opp_name" in locals() else "Opposition"]
-                    ],
-                    hide_index=True,
-                    use_container_width=True
-                )
+            st.dataframe(
+                comparison_df[
+                    ["Ballintubber", "Metric", opp_name if "opp_name" in locals() else "Opposition"]
+                ],
+                hide_index=True,
+                use_container_width=True
+            )
 
 with right_col:
     with st.container(border=True):
