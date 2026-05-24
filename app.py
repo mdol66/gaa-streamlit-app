281065e3c2b7c5c4b140be038b61f549413b3db2
mdol66 <mgd066@gmail.com>
2026-05-24 15:35:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1672,9 +1672,9 @@ with tab0:
                 mode="markers+text",
                 text=bt_misses_h1["__miss_text__"],
                 textposition="middle center",
-                textfont=dict(size=20, color="#888888"),
+                textfont=dict(size=10, color="#888888"),
                 marker=dict(
-                    size=20,
+                    size=10,
                     color="#888888",
                     symbol="x-thin-open"
                 ),
