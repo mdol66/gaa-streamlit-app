5b05723f860d5931e7386bf02b5143beafb21a1b
mdol66 <mgd066@gmail.com>
2026-05-30 15:56:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def build_pitch_shapes() -> list[dict]:
 def add_pitch_labels(fig: go.Figure) -> None:
     fig.add_annotation(
         x=50,
-        y=-5,
+        y=0,
         text="Opposition GOAL",
         showarrow=False,
         font=dict(
@@ -246,7 +246,7 @@ def add_pitch_labels(fig: go.Figure) -> None:
 
     fig.add_annotation(
         x=50,
-        y=105,
+        y=100,
         text="Ballintubber GOAL",
         showarrow=False,
         font=dict(
