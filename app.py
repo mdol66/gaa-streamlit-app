dfea0cc0a2f1a928f1e75298b818f2d432628e9b
mdol66 <mgd066@gmail.com>
2026-05-30 15:42:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2651,6 +2651,19 @@ with tab1:
     with col1:
           
         st.markdown("### Legend")
+        st.markdown(
+            """
+            <div style="
+                font-size:12px;
+                margin-bottom:8px;
+                line-height:1.3;
+            ">
+                ⚪ White border = From Play<br>
+                ⚫ Black border = From Placed
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
         legend_counts = (
             marker_df[cols["outcome"]]
