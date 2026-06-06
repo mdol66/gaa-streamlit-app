a112b0bd18b51d7954bb5f4f85d5e79badaeaad5
mdol66 <mgd066@gmail.com>
2026-06-06 13:19:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -147,12 +147,12 @@ def build_pitch_shapes() -> list[dict]:
     def sy(m: float) -> float:
         return (m / pitch_len) * 100.0
 
-    y13 = sy(13)
-    y20 = sy(20)
-    y45 = sy(45)
-    y50 = 50.0
-    y100_13 = 100.0 - y13
-    y100_20 = 100.0 - y20
+    rx40 = sw(38.0)
+    ry40 = sy(40.0)
+    theta = math.degrees(math.asin(20.0 / 40.0))
+
+    shapes.append(dict(type="path", path=ellipse_arc_path(cx, 0.0, rx40, ry40, theta, 180 - theta), line=line))
+    shapes.append(dict(type="path", path=ellipse_arc_path(cx, 100.0, rx40, ry40, 180 + theta, 360 - theta), line=line))
     y100_45 = 100.0 - y45
 
     large_w = sw(15.0)
