b047e45ba16269b07efe4e88d73369b6e9bbc818
mdol66 <mgd066@gmail.com>
2026-09-14 16:38:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -961,6 +961,7 @@ elif cols["outcome"] is None:
     cols["outcome"] = "__plot_category__"
 
 st.sidebar.header("Filters")
+st.sidebar.caption("Competition filter test")
 
 match_display_choices = []
 team_choices = []
